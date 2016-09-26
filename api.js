d046a6915f4ac879073ef4e6dff5b507700dac97
Alex <adaurkin@rambler.ru>
2016-09-26 14:09:24 +0300

    Reworking api.js

--- api.js
+++ api.js
@@ -3,32 +3,59 @@
 const request = require('request')
 const urlRandomCard = 'http://api.cvd.io/popular/stickers/randomCard/forKeyword/'
 
-module.exports = {
-  getRandomCard: function (keyword, language, callback) {
-    var options = {
-      url: urlRandomCard + encodeURIComponent(keyword),
-      headers: {
-        'Accept': 'application/json',
-        'Accept-Language': language
-      }
+function getRandomCard (keyword, language, callback) {
+  var options = {
+    url: urlRandomCard + encodeURIComponent(keyword),
+    headers: {
+      'Accept': 'application/json',
+      'Accept-Language': language
     }
-
-    request(options, function (error, response, body) {
-      let strContent = ''
-      let strImageLink = ''
-      if (!error && response.statusCode == 200) {
-        strContent = ''
-        if (body > '') {
-          let objBody = JSON.parse(body)
-          strContent = objBody['Content']
-          strImageLink = objBody['ImageLink']
-        }
-      } else {
-        strContent = 'Error\n\n' + JSON.stringify(response) + '\n\n---\n\n' + JSON.stringify(error)
-      }
-      callback(strContent, strImageLink)
-    })
   }
+
+  request(options, function (error, response, body) {
+    let strContent = ''
+    let strImageLink = ''
+    if (!error && response.statusCode == 200) {
+      strContent = ''
+      if (body > '') {
+        let objBody = JSON.parse(body)
+        strContent = objBody['Content']
+        strImageLink = objBody['ImageLink']
+      }
+    } else {
+      strContent = 'Error\n\n' + JSON.stringify(response) + '\n\n---\n\n' + JSON.stringify(error)
+    }
+    callback(strContent, strImageLink)
+  })
+}
+
+module.exports = {
+  getRandomCard: getRandomCard()
+  // getRandomCard: function (keyword, language, callback) {
+  //   var options = {
+  //     url: urlRandomCard + encodeURIComponent(keyword),
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Accept-Language': language
+  //     }
+  //   }
+
+  //   request(options, function (error, response, body) {
+  //     let strContent = ''
+  //     let strImageLink = ''
+  //     if (!error && response.statusCode == 200) {
+  //       strContent = ''
+  //       if (body > '') {
+  //         let objBody = JSON.parse(body)
+  //         strContent = objBody['Content']
+  //         strImageLink = objBody['ImageLink']
+  //       }
+  //     } else {
+  //       strContent = 'Error\n\n' + JSON.stringify(response) + '\n\n---\n\n' + JSON.stringify(error)
+  //     }
+  //     callback(strContent, strImageLink)
+  //   })
+  // }
   ,
   getRandomCardId: function (keyword, language, id, callback) {
     getRandomCard(keyword, language, (strContent, strImageLink) => {
